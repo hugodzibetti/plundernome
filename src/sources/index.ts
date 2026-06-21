e1a537db28e8c4e35b7657a565c903796d957485
hugodzibetti <hugodzibetti@gmail.com>
2026-06-21 18:13:21 +0000

    refactor(sources): consolidate all source defs into single definitions.ts

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,28 +1,5 @@
-import type { SourceDefinition } from '../domain/catalog/types'
-import { FITGIRL_SOURCE } from './fitgirl'
-import { DODI_SOURCE } from './dodi'
-import { STEAMRIP_SOURCE } from './steamrip'
-import { ONLINEFIX_SOURCE } from './onlinefix'
-import { GOGGAMES_SOURCE } from './goggames'
-import { ELAMIGOS_SOURCE } from './elamigos'
-import { GLOAD_SOURCE } from './gload'
-import { OVAGAMES_SOURCE } from './ovagames'
-import { XATAB_SOURCE } from './xatab'
-import { REPACK_GAMES_SOURCE } from './repack-games'
-import { REPACKLAB_SOURCE } from './repacklab'
+import type { SourceDefinition } from '../domain/catalog/types';
+import { SOURCE_DEFINITIONS } from './definitions';
 
-export const SOURCE_DEFINITIONS: SourceDefinition[] = [
-  FITGIRL_SOURCE,
-  DODI_SOURCE,
-  STEAMRIP_SOURCE,
-  ONLINEFIX_SOURCE,
-  GOGGAMES_SOURCE,
-  ELAMIGOS_SOURCE,
-  GLOAD_SOURCE,
-  OVAGAMES_SOURCE,
-  XATAB_SOURCE,
-  REPACK_GAMES_SOURCE,
-  REPACKLAB_SOURCE,
-]
-
-export const BUNDLED_SOURCE_DEFINITIONS = SOURCE_DEFINITIONS
+export { SOURCE_DEFINITIONS };
+export const BUNDLED_SOURCE_DEFINITIONS: SourceDefinition[] = SOURCE_DEFINITIONS;
